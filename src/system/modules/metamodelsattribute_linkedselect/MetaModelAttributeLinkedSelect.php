b4c756851daa385fb57d9e3eb9c152b4bf9ae34d
Stefan Heimes <stefan_heimes@hotmail.com>
2014-06-06 10:27:08 +0200

    Improve the valueToWidget function. Now the function can handle the full data array and the simple version.

--- src/system/modules/metamodelsattribute_linkedselect/MetaModelAttributeLinkedSelect.php
+++ src/system/modules/metamodelsattribute_linkedselect/MetaModelAttributeLinkedSelect.php
@@ -112,9 +112,16 @@ class MetaModelAttributeLinkedSelect extends MetaModelAttributeHybrid
 			return $arrReturn;
 		}
 
-		foreach ($varValue as $arrItem)
+		foreach ($varValue as $mixItem)
 		{
-			$arrReturn[] = $arrItem['id'];
+			if(is_array($mixItem) && isset($mixItem['id']))
+			{
+				$arrReturn[] = $mixItem['id'];
+			}
+			elseif(!is_array($mixItem))
+			{
+				$arrReturn[] = $mixItem;
+			}
 		}
 
 		return $arrReturn;
