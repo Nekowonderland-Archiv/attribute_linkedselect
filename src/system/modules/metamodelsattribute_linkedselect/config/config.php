076a353d3937ed88d74b481fb71d6f41f13f0374
Stefan Heimes <stefan_heimes@hotmail.com>
2014-07-28 18:16:09 +0200

    Add to the select filter.

--- src/system/modules/metamodelsattribute_linkedselect/config/config.php
+++ src/system/modules/metamodelsattribute_linkedselect/config/config.php
@@ -19,3 +19,5 @@ $GLOBALS['METAMODELS']['attributes']['linkedselect'] = array
 	'class' => 'MetaModelAttributeLinkedSelect',
 	'image' => 'system/modules/metamodelsattribute_linkedselect/html/select.png'
 );
+
+$GLOBALS['METAMODELS']['filters']['select']['attr_filter'][] = 'linkedselect';
